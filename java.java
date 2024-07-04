de73da2ac9a9af298f9cff20352c23c82b019c3c
s1310199 <s1310199@u-aizu.ac.jp>
2024-07-04 15:36:02 +0900

    Victory message added

--- java.java
+++ java.java
@@ -1,8 +1,7 @@
 import java.util.Random;
 
 public class java {
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
         Random random = new Random();
         int headsCount = 0;
         int tailsCount = 0;
@@ -21,7 +20,7 @@ public class java {
         }
 
         System.out.println("Heads: " + headsCount + ", Tails: " + tailsCount);
-        
+
         if (headsCount > tailsCount) {
             System.out.println("You won!");
         } else {
