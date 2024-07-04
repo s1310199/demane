0dda53dc6d8f81438105751f9e4b77197a6e09b1
s1310199 <s1310199@u-aizu.ac.jp>
2024-07-04 14:16:20 +0900

    HoT: initial revision

---
+++ java.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class java {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int headsCount = 0;
+        int tailsCount = 0;
+
+        System.out.println("Tossing a coin...");
+
+        for (int round = 1; round <= 3; round++) {
+            boolean isHeads = random.nextBoolean();
+            if (isHeads) {
+                System.out.println("Round " + round + ": Heads");
+                headsCount++;
+            } else {
+                System.out.println("Round " + round + ": Tails");
+                tailsCount++;
+            }
+        }
+
+        System.out.println("Heads: " + headsCount + ", Tails: " + tailsCount);
+    }
+}
