bc1304c881b15c6ae59c31c810cf0e7fc718ff30
s1310199 <s1310199@u-aizu.ac.jp>
2024-07-04 14:52:02 +0900

    User name added

--- java.java
+++ java.java
@@ -5,11 +5,11 @@ public class java {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
-        System.out.print("Who are you? ");
+        System.out.println("Who are you? ");
         String name = scanner.nextLine();
     
         System.out.println("Hello, " + name + "!");
-    
+
         Random random = new Random();
         int headsCount = 0;
         int tailsCount = 0;
@@ -28,7 +28,6 @@ public class java {
         }
 
         System.out.println("Heads: " + headsCount + ", Tails: " + tailsCount);
-        
-        scanner.close();
     }
 }
+
